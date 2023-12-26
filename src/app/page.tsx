5187dbb659b8f08effecc86c6c2c1e72520c9d78
kanaru-lig <sasaki_kanaru@liginc.co.jp>
2023-12-26 14:33:31 +0900

    add caption

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/Button";
 export default function Home() {
   return (
     <main className="mx-auto p-4 max-w-2xl space-y-4">
+      <h3>Material UI デフォルト</h3>
       <div className="space-x-4">
         <MuiButton variant="contained" color="primary">
           ログイン
@@ -15,6 +16,7 @@ export default function Home() {
           ログイン
         </MuiButton>
       </div>
+      <h3>カスタマイズ済み</h3>
       <div className="space-x-4">
         <Button variant="contained" color="primary">
           ログイン
@@ -26,15 +28,16 @@ export default function Home() {
           ログイン
         </Button>
       </div>
+      <h2>size propsをいじった場合</h2>
       <div className="space-x-4">
-        <Button variant="contained" color="primary" size="large">
-          ログイン
+        <Button variant="contained" color="primary" size="small">
+          small
         </Button>
         <Button variant="contained" color="primary" size="medium">
-          ログイン
+          medium
         </Button>
-        <Button variant="contained" color="primary" size="small">
-          ログイン
+        <Button variant="contained" color="primary" size="large">
+          large
         </Button>
       </div>
     </main>
