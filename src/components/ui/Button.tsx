47921d879f74986fe0e69d9eedd288f9c68f1850
kanaru-lig <sasaki_kanaru@liginc.co.jp>
2023-12-26 18:55:28 +0900

    add sizeStyles to change style by size

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,46 +1,108 @@
 import MuiButton, { type ButtonProps } from "@mui/material/Button";
 
 export function Button(props: ButtonProps) {
-  const style = styles[props.variant || "contained"];
+  const eventStyle = eventStyles[props.variant || "contained"];
+  const sizeStyle = sizeStyles[props.size || "medium"];
   return (
     <MuiButton
       sx={{
         boxShadow: "none",
-        borderRadius: 2,
-        ...style,
+        fontWeight: 700,
+        ...eventStyle,
+        ...sizeStyle,
       }}
+      size="medium"
       {...props}
     />
   );
 }
 
-const styles = {
+const sizeStyles = {
+  large: {
+    borderRadius: 2,
+    py: 1,
+    px: 4,
+  },
+  medium: {
+    borderRadius: 2,
+    py: 0.75,
+    px: 2,
+  },
+  small: {
+    borderRadius: 1.5,
+    py: 0.5,
+    px: 1,
+  },
+};
+
+const eventStyles = {
   contained: {
-    "&:hover": { boxShadow: "none", backgroundColor: "primary.A100" },
-    "&:active": { backgroundColor: "primary.A200" },
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "primary.A100",
+      textDecorationColor: "#fff",
+      textDecorationLine: "underline",
+    },
+    "&:active": {
+      backgroundColor: "primary.A200",
+      textDecorationColor: "#fff",
+      textDecorationLine: "underline",
+    },
     "&:focus-visible": {
       boxShadow: "none",
       outline: 2,
       outlineColor: "#B78F00",
       outlineOffset: 1,
+      textDecorationColor: "#fff",
+      textDecorationLine: "underline",
+    },
+    "&:disabled": {
+      backgroundColor: "#AEAEB0",
     },
   },
   outlined: {
-    "&:hover": { boxShadow: "none", backgroundColor: "primary.200" },
-    "&:active": { backgroundColor: "primary.300" },
+    color: "primary.A100",
+    borderColor: "primary.A100",
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "primary.200",
+      textDecorationColor: "primary.A100",
+      textDecorationLine: "underline",
+    },
+    "&:active": {
+      backgroundColor: "primary.300",
+      textDecorationColor: "primary.A100",
+      textDecorationLine: "underline",
+    },
     "&:focus-visible": {
       outline: 2,
       outlineColor: "#B78F00",
       outlineOffset: 1,
+      textDecorationColor: "primary.A100",
+      textDecorationLine: "underline",
     },
   },
   text: {
-    "&:hover": { boxShadow: "none", backgroundColor: "primary.200" },
-    "&:active": { backgroundColor: "primary.300" },
+    color: "primary.A100",
+    textDecorationColor: "primary.A100",
+    textDecorationLine: "underline",
+    "&:hover": {
+      boxShadow: "none",
+      backgroundColor: "primary.200",
+      textDecorationColor: "primary.A100",
+      textDecorationLine: "underline",
+    },
+    "&:active": {
+      backgroundColor: "primary.300",
+      textDecorationColor: "primary.A100",
+      textDecorationLine: "underline",
+    },
     "&:focus-visible": {
       outline: 2,
       outlineColor: "#B78F00",
       outlineOffset: 1,
+      textDecorationColor: "primary.A100",
+      textDecorationLine: "underline",
     },
   },
 };
